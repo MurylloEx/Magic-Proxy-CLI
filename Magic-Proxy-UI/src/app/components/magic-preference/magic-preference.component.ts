22337cb0d2ea8af42b77bfa60785d6c0cfae0975
Muryllo <muryllopimenta@gmail.com>
2021-09-17 23:03:21 -0300

    :sparkles: feature: Finished Preference component.

--- Magic-Proxy-UI/src/app/components/magic-preference/magic-preference.component.ts
+++ Magic-Proxy-UI/src/app/components/magic-preference/magic-preference.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MagicPreferenceComponent implements OnInit {
 
+  m_AllowUnknownHosts: boolean = false;
+  m_AllowWebsockets: boolean = false;
+  m_HttpEnabled: boolean = false;
+  m_HttpsEnabled: boolean = false;
+  m_HstsEnabled: boolean = false;
+  m_HttpPort: number = 80;
+  m_HttpsPort: number = 443;
+
   constructor() { }
 
   ngOnInit(): void {
